8ffbb7c4b7a33bc0bd8d125cd677e426c72def4e
Jonathan Rivera <jrivera157@bloomberg.net>
2024-03-05 03:03:57 -0500

    completing the lab

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -14,26 +14,33 @@
 
 import argparse
 import sys
+import time
 
 from solution.consumer_sol import mqConsumer  # pylint: disable=import-error
 
 def main(sector: str, queueName: str) -> None:
     
     # Implement Logic to Create Binding Key from the ticker and sector variable -  Step 2
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    bindingKey = f'{sector}.#'
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
-    consumer.startConsuming()
-    
+    consumer.startConsuming()    
 
 
 if __name__ == "__main__":
 
     # Implement Logic to read the sector and queueName string from the command line and save them - Step 1
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    if len(sys.argv) != 3:
+        print(f'Error, wrong number of arguments. Needs args in the format of: consume.py <sector> <queueName>. Only got {len(sys.argv)} args')
+        sys.exit(-3)
 
+    sector = sys.argv[1]
+
+    # i could pass queue name here although i feel lazy so to make sure i never mess up, i could just pass in a timestamp so it's always unique 
+    # queue = sys.argv[2]
+
+    fake_name = f'{time.time()}'
+    print(f'faking the queue name with seconds from epoch: {fake_name}')
+    queue = fake_name
+   
     sys.exit(main(sector,queue))
--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -21,27 +21,38 @@ from solution.producer_sol import mqProducer  # pylint: disable=import-error
 def main(ticker: str, price: float, sector: str) -> None:
     
     # Implement Logic to Create Routing Key from the ticker and sector variable -  Step 2
-    #
-    #                       WRITE CODE HERE!!!
-    #
-
+    routingKey = f'{sector}.{ticker}'
 
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
 
 
     # Implement Logic To Create a message variable from the variable EG. "TSLA price is now $500" - Step 3
-    #
-    #                       WRITE CODE HERE!!!
-    #
-    
+    message = f'{ticker} price is now ${price}, from the {sector} sector'    
     
     producer.publishOrder(message)
 
 if __name__ == "__main__":
 
     # Implement Logic to read the ticker, price and sector string from the command line and save them - Step 1
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    
+    # print("Name of the program using sys.argv[0]: ", sys.argv[0])
+    # print("Length of arguments given including program name: ", len(sys.argv))
+    # print("Argument list: ", sys.argv)
+    # print("Argument list type: ", type(sys.argv))
+    # print("Give the first argument (after program name): ", sys.argv[1])
+
+    if len(sys.argv) != 4:
+        print(f'Error, wrong number of arguments. Needs args in the format of: publish.py <ticker> <price> <sector>. Only got {len(sys.argv)} args')
+        sys.exit(-3)
+
+    ticker = sys.argv[1]
+    price = sys.argv[2]
+    try:
+        price = float(price)
+    except Exception:
+        print(f'Error, price arg could not be formatted as a float: {sys.argv[2]}')
+        sys.exit(-4)
+
+    sector = sys.argv[3]
 
     sys.exit(main(ticker,price,sector))
